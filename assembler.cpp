04f292332c02b3d454f4c5cbb410d8c6f5b216d2
LomininD <saimon1402@mail.ru>
2025-10-07 09:23:48 +0300

    Added ability to pass input and output file name via command line argument, the assembler reads from input_file and puts byte code in the output_file, mass refactoring of assembler: added structures and new functions

--- assembler.cpp
+++ assembler.cpp
@@ -7,60 +7,84 @@
 #include "stack/font_styles.h"
 #include "processor_properties.h"
 
+struct files_info
+{
+    char* input_file_name;
+    char* output_file_name;
+    FILE* input_file;
+    FILE* output_file;
+};
+
+struct assembler_info
+{
+    size_t len;
+    char* str;
+    char raw_cmd[32];
+    proc_commands cmd;
+    int end;
+};
+
+struct debug_info
+{
+    int current_line;
+    int got_hlt;
+    int got_out;
+    int got_something;
+};
+
+// if no argument for output is given, output to file with the same name but .out extention
 // solve problem with unexpected symbols
-// add output to program.out
-// add file paths
 // debug messages in console (in debug mode) - user can redirect them to file
 // debug messages to log file (always)
 // errors and introductory information in console (always)
 
+err_t initialise_assembler(int argc, char* argv[], files_info* files, assembler_info* asm_data, debug_info* debug);
+err_t determine_cmd(files_info* files, assembler_info* asm_data, debug_info* debug);
+err_t read_arg(files_info* files, assembler_info* asm_data, debug_info* debug, int* number);
+void readline(assembler_info* asm_data, files_info* file);
+void check_warnings(debug_info* debug, files_info* files);
 
-err_t determine_cmd(const char* file_name, int current_line, char* raw_cmd, proc_commands* cmd, char* str);
-err_t read_arg(const char* file_name, int current_line, int* number, char* str);
-char* readline(char* str, size_t* len, int* end);
-
-int main()
+int main(int argc, char* argv[])
 {
     printf(MAKE_BOLD("+++ ASSEMBLER +++\n\n"));
     printf("version: %d\n", version);
     printf("designed by LMD (c) \n\n");
 
+    files_info files = {};
+    assembler_info asm_data = {};
+    debug_info debug = {};
+
+    err_t initialised = initialise_assembler(argc, argv, &files, &asm_data, &debug);
+    if (initialised != ok)
+    {
+        printf("\n");
+        printf("assembler: " MAKE_BOLD_RED("aborting due to error\n"));
+        return 0;
+    }
+
     printf("assembler: began assembly\n\n");
 
-    int current_line = 0;
-    const char* file_name = "program.asm";
-
-    size_t len = 50;
-    char* str = (char*) calloc(len + 1, sizeof(char));
-    char raw_cmd[32] = {};
-    proc_commands cmd = UNKNOWN;
-    int end = 0;
-
-    int got_hlt = 0;
-    int got_out = 0;
-    int got_something = 0;
-
-    while (!end)
+    while (!asm_data.end)
     {
-        current_line++;
-        str = readline(str, &len, &end);
+        debug.current_line++;
+        readline(&asm_data, &files);
 
-        if (end)
+        if (asm_data.end)
             break;
 
-        got_something = 1;
-        printf("assembler: got str: %s\n", str);
-        int scanned = sscanf(str, "%31s", raw_cmd);
+        debug.got_something = 1;
+        printf("assembler: got str: %s\n", asm_data.str);
 
+        int scanned = sscanf(asm_data.str, "%31s", asm_data.raw_cmd);
         if (scanned == -1)
         {
             printf("\n");
             continue;
         }
 
-        printf("assembler: got instruction: %s\n", raw_cmd);
-        err_t recognized = determine_cmd(file_name, current_line, raw_cmd, &cmd, str);
+        printf("assembler: got instruction: %s\n", asm_data.raw_cmd);
 
+        err_t recognized = determine_cmd(&files, &asm_data, &debug);
         if (recognized != ok)
         {
             printf("\n");
@@ -70,172 +94,239 @@ int main()
 
         printf("\n");
 
-        if (cmd == HLT)
+        if (asm_data.cmd == HLT)
         {
-            got_hlt = 1;
+            debug.got_hlt = 1;
             break;
         }
 
-        if (cmd == OUT)
-            got_out = 1;
+        if (asm_data.cmd == OUT)
+            debug.got_out = 1;
         else
-            got_out = 0;
+            debug.got_out = 0;
     }
 
-    free(str);
+    check_warnings(&debug, &files);
 
-    if (got_something)
-    {
-        if (!got_out)
-        {
-            printf("assembler: %s:%d: " MAKE_BOLD("WARNING:") " calculations seem to have no effect\n", file_name, current_line);
-            printf(MAKE_GREY("Note: you may forgot OUT instruction in your code\n\n"));
-        }
-
-        if (!got_hlt)
-            printf("assembler: %s:%d: " MAKE_BOLD("WARNING:") " no HLT instruction in the end of the program\n\n", file_name, current_line);
-    }
-    else
-    {
-        printf("assembler: " MAKE_BOLD("WARNING:") " got blank file\n\n");
-    }
-
+    free(asm_data.str);
     printf("assembler: finished assembly\n");
+    fclose(files.input_file);
+    fclose(files.output_file);
     return 0;
 }
 
 
-err_t read_arg(const char* file_name, int current_line, int* number, char* str)
+err_t initialise_assembler(int argc, char* argv[], files_info* files, assembler_info* asm_data, debug_info* debug)
 {
-    assert(number != NULL);
-    assert(str != NULL);
+    assert(argv != NULL);
+    assert(files != NULL);
+    assert(asm_data != NULL);
+    assert(debug != NULL);
 
-    int success = sscanf(str, "%*s %d", number);
+    printf("initialise_assembler: began initialising\n");
+
+    files->output_file_name = (char*) "program.out";
+
+    asm_data->len = 50;
+    asm_data->str = (char*) calloc(asm_data->len + 1, sizeof(char));
+    asm_data->cmd = UNKNOWN;
+    asm_data->end = 0;
+
+    debug->current_line = 0;
+    debug->got_hlt = 0;
+    debug->got_out = 0;
+    debug->got_something = 0;
+
+    if (argc < 2)
+    {
+        printf("initialise_assembler: " MAKE_BOLD_RED("ERROR:") " required files were not provided\n");
+        return error;
+
+    }
+    else if (argc > 3)
+    {
+        printf("initialise_assembler: " MAKE_BOLD_RED("ERROR:") " got to many files\n");
+        return error;
+    }
+    else
+    {
+        files->input_file_name = argv[1];
+        files->input_file = fopen(files->input_file_name, "r");
+
+        if (argc == 3)
+            files->output_file_name = argv[2];
+
+        files->output_file = fopen(files->output_file_name, "w");
+
+        if (files->input_file == NULL || files->output_file == NULL)
+        {
+            printf("initialise_assembler: " MAKE_BOLD_RED("ERROR:") " a problem with opening files occurred\n");
+            printf(MAKE_GREY("Note: file name is likely to be incorrect\n"));
+            return error;
+        }
+
+        printf("initialise_assembler: finished initialising\n");
+        return ok;
+    }
+}
+
+
+err_t read_arg(files_info* files, assembler_info* asm_data, debug_info* debug, int* number)
+{
+    assert(files != NULL);
+    assert(asm_data != NULL);
+    assert(debug != NULL);
+    assert(number != NULL);
+
+    int success = sscanf(asm_data->str, "%*s %d", number);
 
     if (success == 1)
     {
         printf("read_arg: recognised arg %d\n", *number);
-        printf("%d\n", *number);
+        fprintf(files->output_file, "%d\n", *number);
         return ok;
     }
     else
     {
         printf("\n");
-        printf("read_arg: %s:%d: " MAKE_BOLD_RED("ERROR:") " failed to get arg\n", file_name, current_line);
+        printf("read_arg: %s:%d: " MAKE_BOLD_RED("ERROR:") " failed to get arg\n", files->input_file_name, debug->current_line);
         return wrong_number;
     }
 }
 
 
-err_t determine_cmd(const char* file_name, int current_line, char* raw_cmd, proc_commands* cmd, char* str)
+err_t determine_cmd(files_info* files, assembler_info* asm_data, debug_info* debug)
 {
-    assert(raw_cmd != NULL);
-    assert(cmd != NULL);
-    assert(str != NULL);
+    assert(files != NULL);
+    assert(asm_data != NULL);
+    assert(debug != NULL);
 
-    if (strcmp("PUSH", raw_cmd) == 0)
+    if (strcmp("PUSH", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized push\n");
 
+        fprintf(files->output_file, "%d\n", PUSH);
+        asm_data->cmd = PUSH;
+
         int number = 0;
-        err_t is_read = read_arg(file_name, current_line, &number, str);
+        err_t is_read = read_arg(files, asm_data, debug, &number);
 
         if (is_read != ok)
             return error;
 
-        printf("%d\n", PUSH);
-        *cmd = PUSH;
-
         return ok;
     }
-    else if (strcmp("ADD", raw_cmd) == 0)
+    else if (strcmp("ADD", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized add\n");
-        printf("%d\n", ADD);
-        *cmd = ADD;
+        fprintf(files->output_file, "%d\n", ADD);
+        asm_data->cmd = ADD;
 
         return ok;
     }
-    else if (strcmp("SUB", raw_cmd) == 0)
+    else if (strcmp("SUB", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized sub\n");
-        printf("%d\n", SUB);
-        *cmd = SUB;
+        fprintf(files->output_file, "%d\n", SUB);
+        asm_data->cmd = SUB;
         return ok;
     }
-    else if (strcmp("DIV", raw_cmd) == 0)
+    else if (strcmp("DIV", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized div\n");
-        printf("%d\n", DIV);
-        *cmd = DIV;
+        fprintf(files->output_file, "%d\n", DIV);
+        asm_data->cmd = DIV;
         return ok;
     }
-    else if (strcmp("MULT", raw_cmd) == 0)
+    else if (strcmp("MULT", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized mult\n");
-        printf("%d\n", MULT);
-        *cmd = MULT;
+        fprintf(files->output_file, "%d\n", MULT);
+        asm_data->cmd = MULT;
         return ok;
     }
-    else if (strcmp("SQRT", raw_cmd) == 0)
+    else if (strcmp("SQRT", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized sqrt\n");
-        printf("%d\n", SQRT);
-        *cmd = SQRT;
+        fprintf(files->output_file, "%d\n", SQRT);
+        asm_data->cmd = SQRT;
         return ok;
     }
-    else if (strcmp("OUT", raw_cmd) == 0)
+    else if (strcmp("OUT", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized out\n");
-        printf("%d\n", OUT);
-        *cmd = OUT;
+        fprintf(files->output_file, "%d\n", OUT);
+        asm_data->cmd = OUT;
         return ok;
     }
-    else if (strcmp("HLT", raw_cmd) == 0)
+    else if (strcmp("HLT", asm_data->raw_cmd) == 0)
     {
         printf("determine_cmd: recognized hlt\n");
-        printf("%d\n", HLT);
-        *cmd = HLT;
+        fprintf(files->output_file, "%d\n", HLT);
+        asm_data->cmd = HLT;
         return ok;
     }
 
     printf("\n");
-    printf("determine_cmd: %s:%d: " MAKE_BOLD_RED("ERROR:") " invalid command (%s)\n", file_name, current_line, raw_cmd);
-    *cmd = UNKNOWN;
+    printf("determine_cmd: %s:%d: " MAKE_BOLD_RED("ERROR:") " invalid command (%s)\n", files->input_file_name, debug->current_line, asm_data->raw_cmd);
+    asm_data->cmd = UNKNOWN;
     return error;
 }
 
 
-char* readline(char* str, size_t* len, int* end)
+void readline(assembler_info* asm_data, files_info* file)
 {
+    assert(asm_data != NULL);
+    assert(file != NULL);
+
     size_t symbol_ind = 0;
     char c = 0;
 
     while (true)
     {
-        c = getchar();
+        c = fgetc(file->input_file);
 
         if (c == '\n' || c == '\r' || c == EOF)
         {
             if (c == EOF)
-                *end = 1;
+                asm_data->end = 1;
             break;
         }
 
         if (c == ';')
-            *(str + symbol_ind) = '\0';
+            *(asm_data->str + symbol_ind) = '\0';
         else
-            *(str + symbol_ind) = toupper(c);
+            *(asm_data->str + symbol_ind) = toupper(c);
 
         symbol_ind++;
 
-        if (symbol_ind == *len)
+        if (symbol_ind == asm_data->len)
         {
-            *len *= 2;
-            str = (char*) realloc(str, (*len + 1) * sizeof(char));
+            asm_data->len *= 2;
+            asm_data->str = (char*) realloc(asm_data->str, (asm_data->len + 1) * sizeof(char));
         }
     }
 
-    *(str + symbol_ind) = '\0';
-    return str;
+    *(asm_data->str + symbol_ind) = '\0';
+    //return asm_data->str; // ????
+}
+
+
+void check_warnings(debug_info* debug, files_info* files)
+{
+    if (debug->got_something)
+    {
+        if (!debug->got_out)
+        {
+            printf("assembler: %s:%d: " MAKE_BOLD("WARNING:") " calculations seem to have no effect\n", files->input_file_name, debug->current_line);
+            printf(MAKE_GREY("Note: you may forgot OUT instruction in your code\n\n"));
+        }
+
+        if (!debug->got_hlt)
+            printf("assembler: %s:%d: " MAKE_BOLD("WARNING:") " no HLT instruction in the end of the program\n\n", files->input_file_name, debug->current_line);
+    }
+    else
+    {
+        printf("assembler: " MAKE_BOLD("WARNING:") " got blank file\n\n");
+    }
 }
